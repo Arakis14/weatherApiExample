05e5bdb281d3c379fca33eb0b2c975e7c9931614
Arakis14 <wiktor.lasota@vp.pl>
2022-06-16 18:33:56 +0200

    Store API key

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
+mod key;
+/*
+pub fn key() -> &'static str {
+  return "API_KEY";
+}
+*/
 use reqwest::Result;
 //use serde::Deserialize;
-
 /*
 #[derive(Deserialize, Debug)]
-struct Weather {
-    body: String,
-    content: String,
+struct WeatherData {
+    location: {
+        name: String,
+        region: String,
+        country: String,
+        lat: f64,
+        lon: f64,
+        tz_id: String,
+        location_epoch: i64,
+        location_time: String,
+    }
 }
 */
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
-    let response = reqwest::get("http://api.weatherapi.com/v1/current.json?key=2e280afadc90462bba4194432221406&q=London")
+    let api_key = key::key();
+    let request = format!("http://api.weatherapi.com/v1/current.json?key={}{}", api_key,"&q=London");
+    let response = reqwest::get(request)
         .await?
         .text()
         .await?;
